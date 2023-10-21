d7b7207b6b6436f806646e40d7a42d6266a542a1
Will Garside <worgarside@gmail.com>
2023-10-21 14:28:44 +0100

    Run PCHs

--- apps/truelayer/bank_balance.py
+++ apps/truelayer/bank_balance.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from typing import Any, Literal, overload
 
 # pylint: disable=no-name-in-module
-from appdaemon.plugins.hass.hassapi import Hass  # type: ignore[import]
+from appdaemon.plugins.hass.hassapi import Hass  # type: ignore[import-not-found]
 from wg_utilities.clients import TrueLayerClient
 from wg_utilities.clients.truelayer import Account, Bank, Card
 from wg_utilities.loggers import add_warehouse_handler
